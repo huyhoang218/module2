c41d2cdd3833af1f025d3e69409150f539e3536f
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-05-09 21:36:51 +0700

    update delete, add element

---
+++ session3_array/src/AddElement.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class AddElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length;
+        //Nhập độ dài mảng
+        do {
+            System.out.print("Enter length of array: ");
+            length = input.nextInt();
+        } while (length <= 0);
+
+        int[] array = new int[length];
+        int element;
+        //Nhập phần tử trong mảng
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("element " + i + " = ");
+            element = input.nextInt();
+            array[i] = element;
+        }
+
+        System.out.print("Add element: ");
+        int add = input.nextInt();
+        System.out.print("Index: ");
+        int index = input.nextInt();
+
+        if (index <= -1 || index >= (array.length - 1)) {
+            System.out.println("Không chèn được vào mảng");
+        } else {
+            add(array, add, index);
+        }
+    }
+
+    public static void add(int[] array, int add, int index) {
+        int[] newArray = new int[array.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[index] = add;
+        for (int i = index + 1; i < newArray.length; i++) {
+            newArray[i] = array[i - 1];
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+    }
+}
---
+++ session3_array/src/DeleteElement.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DeleteElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] array = {1, 56, 4, -4, 6, 4, 73, 4};
+        System.out.print("Nhập phần tử muốn xóa: ");
+        int x = input.nextInt();
+        // Đếm số lượng phần tử cùng giá trị với phần tử xóa
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (x == array[i]) {
+                count++;
+            }
+        }
+
+        int[] newArray = new int[array.length - count];
+        int index = 0;
+        // Xóa các phần tử
+        for (int i = 0; i < array.length; i++) {
+            if(x != array[i]) {
+                newArray[index] = array[i];
+                index++;
+            }
+        }
+        // in ra mảng mới
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+    }
+}
