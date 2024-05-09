787be851c093493c3be3faa90b0a8ba4df5da999
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-05-09 22:03:47 +0700

    update concat array

---
+++ session3_array/src/MergeArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MergeArray {
+    public static void main(String[] args) {
+        int[] arrayFirst = new int[5];
+        System.out.println("Enter array 1: ");
+        inputElement(arrayFirst);
+        System.out.println("Enter array 2: ");
+        int[] arrayLast = new int[4];
+        inputElement(arrayLast);
+
+        int length = arrayFirst.length + arrayLast.length;
+        int[] array = new int[length];
+
+        for (int i = 0; i < arrayFirst.length; i++) {
+            array[i] = arrayFirst[i];
+        }
+        int j = 0;
+        for (int i = arrayFirst.length; i < length; i++) {
+            array[i] = arrayLast[j];
+            j++;
+        }
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void inputElement(int[] array) {
+        Scanner input = new Scanner(System.in);
+        int element;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("index " + i + " = ");
+            element = input.nextInt();
+            array[i] = element;
+        }
+    }
+}
