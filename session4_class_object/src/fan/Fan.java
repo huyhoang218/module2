d8ee7ea3708778d9d610a72207e12224c5b463dc
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-05-11 23:31:13 +0700

    package

--- session4_class_object/src/fan/Fan.java
+++ session4_class_object/src/fan/Fan.java
@@ -1,3 +1,5 @@
+package fan;
+
 public class Fan {
     final int SLOW = 1;
     final int MEDIUM = 2;
@@ -34,23 +36,5 @@ public class Fan {
             return "speed = " + speed + " color = " + color + " radius = " + radius + " fan is on";
         }
         return "speed = " + speed + " color = " + color + " radius = " + radius + " fan is off";
-    }
-
-    public static void main(String[] args) {
-        Fan fan1 = new Fan();
-        fan1.setOn();
-        fan1.setSpeed(3);
-        fan1.setRadius(10);
-        fan1.setColor("yellow");
-
-        System.out.println(fan1.toString());
-
-        Fan fan2 = new Fan();
-        fan2.setOff();
-        fan2.setSpeed(2);
-        fan2.setRadius(4);
-        fan2.setColor("blue");
-
-        System.out.println(fan2.toString());
     }
 }
