ea3fcf903708626c80947400d4ef4cc3d1579937
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-05-28 15:51:04 +0700

    update

--- session15_exception_debug/src/triangleException/Triangle.java
+++ session15_exception_debug/src/triangleException/Triangle.java
@@ -1,13 +1,19 @@
 package triangleException;
 
-public class Triangle {
+public class Triangle{
     private double side1;
     private double side2;
     private double side3;
 
     public Triangle() {}
 
-    public Triangle(double side1, double side2, double side3){
+    public Triangle(double side1, double side2, double side3) throws TriangleExeption{
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            throw new TriangleExeption("Cạnh phải lớn hơn 0 ");
+        }
+        if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+            throw new TriangleExeption("Tổng 2 cạnh phải lớn hơn cạnh còn lại");
+        }
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
